170ef4b78d43c9e6746f3a07ecc55abdd774d9e8
Mark Mulder <markmulder@gmail.com>
2015-06-10 11:35:24 +0200

    Set the required var in test

--- push_notifier_test.go
+++ push_notifier_test.go
@@ -4,18 +4,23 @@ import (
 	"io/ioutil"
 	"net/http"
 	"net/http/httptest"
+	"os"
 	"strings"
 	"testing"
 
 	"github.com/bittersweet/push_notifier/Godeps/_workspace/src/github.com/stretchr/testify/assert"
 )
 
+func init() {
+	os.Setenv("HOOK_SECRET", "secret")
+}
+
 func TestReceiveWithCorrectSignature(t *testing.T) {
 	file, _ := ioutil.ReadFile("./fixtures/payload2.json")
 	payload := strings.Split(string(file), "\n")[0] // strip off the newline
 	url := "/receive"
 	request, _ := http.NewRequest("POST", url, strings.NewReader(string(payload)))
-	request.Header.Add("X-Hub-Signature", "sha1=78dfb5f57f2673b1e006dddb21297834db260813")
+	request.Header.Add("X-Hub-Signature", "sha1=6f64f4be5185c80b52eea72fe120f9f0b5ce28c7")
 	response := httptest.NewRecorder()
 	handleReceive(response, request)
 
@@ -26,7 +31,7 @@ func TestReceiveWithInCorrectSignature(t *testing.T) {
 	payload := `{"payload": "incorrect"}`
 	url := "/receive"
 	request, _ := http.NewRequest("POST", url, strings.NewReader(payload))
-	request.Header.Add("X-Hub-Signature", "sha1=78dfb5f57f2673b1e006dddb21297834db260813")
+	request.Header.Add("X-Hub-Signature", "sha1=incorrectsignature")
 	response := httptest.NewRecorder()
 	handleReceive(response, request)
 
